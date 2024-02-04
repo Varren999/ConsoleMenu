6a81ee0a897388d74d336ed96b0936e5feed4fb1
Varren999 <varren999@gmail.com>
2024-02-04 11:08:25 +0700

    changed project -> dll

--- UI С#/main.cs
+++ UI С#/main.cs
@@ -3,22 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleMenu;
 using UI_С_;
 
 class Program
 {
-    static void foo()
-    {
-        Console.WriteLine("Функция foo");
-    }
-    static void bar()
-    {
-        Console.WriteLine("Функция bar");
-    }
-
     static void Main()
     {
-        Menu menu = new Menu("Тестовое меню", new string[] { "foo", "bar" }, new List<Menu.delFunction> {foo, bar }) ;
+        testClass test = new testClass();
+        Menu menu = new Menu("Тестовое меню", new string[] { "foo", "bar" }, new List<Menu.delFunction> { test.foo, test.bar });
         menu.Start();
+
     }
 }
---
+++ UI С#/testClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_С_
+{
+    internal class testClass
+    {
+        public void foo()
+        {
+            Console.WriteLine("Функция foo");
+        }
+        public void bar()
+        {
+            Console.WriteLine("Функция bar");
+        }
+    }
+}
